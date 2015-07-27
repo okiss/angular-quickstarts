60d57dc34f2cad14958c2ff3fe9bec8ab85094ed
Oliver Kiss <okiss@redhat.com>
2015-07-27 14:16:01 +0200

    Set script execution timeout

--- kitchensink-angularjs-bootstrap/functional-tests/src/test/java/org/jboss/as/quickstarts/kitchensink/test/KitchensinkAngularjsBootstrapTest.java
+++ kitchensink-angularjs-bootstrap/functional-tests/src/test/java/org/jboss/as/quickstarts/kitchensink/test/KitchensinkAngularjsBootstrapTest.java
@@ -32,6 +32,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -128,6 +129,8 @@ public class KitchensinkAngularjsBootstrapTest {
 
     @Before
     public void loadPage() {
+        browser.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+
         browser.get(contextPath.toString());
         form.waitUntilPresent();
     }
